7a95970a01993ad991407bbb8540bffb8900f4bf
ANDY-PC\ANDY <nguyenngoctan.mobile@gmail.com>
2018-01-04 14:26:03 +0700

    updateport3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var hellodata =require('./server_routers/api/hellodata');
 
 var app = express();
 app.use(express.static(`${__dirname}/ui-react/build`));
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8081
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 app.use("/api",hellodata);
 app.listen(port,function(){
     console.log("==========Server API is already===========");
